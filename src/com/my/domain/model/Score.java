0d724d323a5b0f594403e8c4b9c5dd123cbe8664
KevinBrother <1301239018@qq.com>
2017-06-01 13:35:37 +0800

    homework

---
+++ src/com/my/domain/model/Score.java
@@ -0,0 +1,5 @@
+package com.my.domain.model;
+
+public class Score {
+
+}
---
+++ src/com/my/domain/model/Students.java
@@ -0,0 +1,11 @@
+package com.my.domain.model;
+
+import java.util.Date;
+
+public class Students {
+	private int id;
+	private int classId;
+	private String name;
+	private Date birthday;
+	private String password;
+}
