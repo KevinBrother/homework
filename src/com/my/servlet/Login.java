e7ebcf5931ff038034fc52a1648b60984d82ad21
KevinBrother <1301239018@qq.com>
2017-06-08 11:02:49 +0800

    commit test

--- src/com/my/servlet/Login.java
+++ src/com/my/servlet/Login.java
@@ -19,7 +19,7 @@ public class Login extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
-		this.doGet(req, resp);
+		this.doPost(req, resp);
 	
 	}
 	
