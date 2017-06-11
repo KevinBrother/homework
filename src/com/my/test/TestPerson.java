b73d4cfc7027eef9626cad6be876a65b03f1de63
KevinBrother <1301239018@qq.com>
2017-06-11 17:27:34 +0800

    结果集预处理 和数据的json处理

---
+++ src/com/my/test/TestPerson.java
@@ -0,0 +1,37 @@
+package com.my.test;
+
+public class TestPerson {
+	private int age;
+	
+	private String name;
+
+	public TestPerson() {
+
+	}
+
+	public TestPerson(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "TestPerson [age=" + age + ", name=" + name + "]";
+	}
+}
---
+++ src/com/my/util/ModelConvert.java
@@ -0,0 +1,26 @@
+package com.my.util;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class ModelConvert {
+	
+	public static List<Map<String, Object>> convertList(ResultSet rs) {
+		
+		//数组的第一个元素
+		
+		//每个元素一共多少列,做为map的key
+		
+		//然后给map中相应的key赋值
+		
+		//把map加到list中
+		
+		
+		//http://blog.csdn.net/laonayonghaifeisi/article/details/52563895?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io
+		return null;
+	
+	}
+}
